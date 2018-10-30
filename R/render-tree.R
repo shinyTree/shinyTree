2e27cc593408f90843659b7c8d62b5b9864ec55c
Michael A Bell <bellma@lilly.com>
2018-10-30 16:18:48 -0400

    #49

--- R/render-tree.R
+++ R/render-tree.R
@@ -13,14 +13,17 @@ renderTree <- function(expr, env = parent.frame(), quoted = FALSE){
   func <- shiny::exprToFunction(expr, env, quoted)
   return(function(shinysession, name, ...) {
     tree <- func()
-    
-    shiny::HTML(as.character(listToTags(tree)))
+    updateTree(shinysession,name,tree) 
+    NULL
   })
 }
 
 #' Render an empty ShinyTree
 #' 
 #' Renders a tree with no defined nodes.
+#' @param env The environment in which \code{expr} should be evaluated.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#' is useful if you want to save an expression in a variable.
 #' \code{\link{shinyTree}}.
 #'
 #' @seealso \code{\link{shinyTree}}
