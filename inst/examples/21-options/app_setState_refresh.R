6b703ef2cc56fd7ca2f8c35bb738f28446e692e0
thothal <foren@thothal.com>
2019-11-06 17:55:15 +0100

    Update option example to highlight difference
    In the previous version the user needed to hardcode the options,
    to see any difference. This is now done via shiny controls,
    illustrating better what the benefit of the options are.

--- inst/examples/21-options/app_setState_refresh.R
+++ inst/examples/21-options/app_setState_refresh.R
@@ -3,16 +3,18 @@ library(shinyTree)
 
 ## OPTIONS #####################
 options(shinyTree.setState = FALSE)
-# options(shinyTree.refresh = TRUE)
+options(shinyTree.refresh  = FALSE)
 
 ## TREE DATA ###################
-tree = list(a=list(a1=1,a2=2) , b="b") 
-tree = lapply(tree, function(x) structure(x, stopened = T))
+tree <- list(a = list(a1 = 1, a2 = 2), b = "b") 
+tree <- lapply(tree, function(x) structure(x, stopened = TRUE))
 
 ## UI ######################
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
+      checkboxInput("refresh", "options(shinyTree.refresh)"),
+      checkboxInput("set_state", "options(shinyTree.setState)"),
       actionButton('reset', 'Reset nodes'),
       hr(),
       helpText(HTML(
@@ -25,7 +27,7 @@ ui <- fluidPage(
         ))
     ),
     mainPanel(
-      shinyTree("tree", ),
+      uiOutput("tree_placeholder"),
       hr(),
       h4("Selected nodes:"),
       verbatimTextOutput("idSelected")
@@ -35,18 +37,25 @@ ui <- fluidPage(
 
 ## Server ######################
 server <- function(input, output, session) {
-  output$tree = renderTree({
-    attr(tree[[1]][[2]], "stselected") <- TRUE
-    tree
-  })
-  
+
+
   output$idSelected <- renderPrint({
-    str(get_selected(input$tree, format = "classid"))
+    str(get_selected(req(input$tree), format = "classid"))
   })
   
   # An observer is used to trigger a tree update when reset is clicked.
   observeEvent(input$reset, {
-    updateTree(session,"tree", data = tree)
+    updateTree(session, "tree", data = tree)
+  })
+  
+  observe({
+    options(shinyTree.setState = input$set_state)
+    options(shinyTree.refresh = input$refresh)
+    output$tree_placeholder <- renderUI(shinyTree("tree"))
+    output$tree <- renderTree({
+      attr(tree[[1]][[2]], "stselected") <- TRUE
+      tree
+    })
   })
 }
 
