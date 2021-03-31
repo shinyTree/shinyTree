44f22ec6d25faf00c45a31536befbf6fada03ae1
Sandro Raabe <s.raabe@consus-clinicmanagement.de>
2021-03-31 09:14:51 +0200

    set node attributes for tree
    usecase: file structure tree with icons

---
+++ R/tree_traversal.R
@@ -0,0 +1,37 @@
+#' Tree traversal
+#'
+#' Traverse through tree/list to set node attributes, e.g. change icons.
+#' Useful for directory structure icons where inner nodes are directories, leafs are files.
+#' 
+#' @param tree named nested list
+#' @param attr_name name of attribute to set
+#' @param inner_val value of attribute for inner tree nodes
+#' @param leaf_val value of attribute for outer tree nodes
+#' @examples
+#'\dontrun{
+#' tree <- dfToTree(data.frame(Titanic),  c("Sex", "Survived"))
+#' str(set_node_attrs(tree, attr_name = "sttype", inner_val = "directory", leaf_val = "file"))
+#'}
+#' @return named nested list
+#' @export
+set_node_attrs <- function(tree, attr_name, inner_val, leaf_val){
+  if(length(tree) == 0 || is.atomic(tree)){ # trifft leaves der Form "" oder list()
+    set_attr(tree, attr_name, leaf_val)
+  }else{
+    res <- lapply(tree, set_node_attrs, attr_name, inner_val, leaf_val)
+    set_attr(res, attr_name, inner_val)
+  }
+}
+
+#' Set attribute on object
+#'
+#  Set attribute on object and return the object
+#' @param obj any object
+#' @param attr_name name of attribute to set
+#' @param attr_value value of attribute to set
+#' @return the same object with set attribute
+set_attr <- function(obj, attr_name, attr_value){
+  # set attribute on object and return the object
+  attr(obj, attr_name) <- attr_value
+  obj
+}
---
+++ tests/testthat/tree_traversal.R
@@ -0,0 +1,30 @@
+test_that("chaning attribute for object works", {
+  obj <- data.frame()
+  obj <- set_attr(obj, "testAttr", "testVal")
+  expect_true("testAttr" %in% names(attributes(obj)))
+  expect_equal(attr(obj, "testAttr"), "testVal")
+})
+
+test_that("changing icons for file structure tree works",{
+  tree <- list(
+    "folder1" = list(
+      "subfolder1" = list(
+        "file1" = "",
+        "file2" = ""
+      ),
+      "file3" = ""),
+    "folder2" = list(
+      "file4" = "",
+      "file5" = "")
+  )
+  
+  tree_with_icons <- set_node_attrs(tree, attr_name = "sttype", inner_val = "directory", leaf_val = "file")
+  expect_equal(attr(tree_with_icons$folder1$subfolder1$file1, "sttype"), "file")
+  expect_equal(attr(tree_with_icons$folder1$subfolder1$file2, "sttype"), "file")
+  expect_equal(attr(tree_with_icons$folder1$file3, "sttype"), "file")
+  expect_equal(attr(tree_with_icons$folder2$file4, "sttype"), "file")
+  expect_equal(attr(tree_with_icons$folder2$file5, "sttype"), "file")
+  expect_equal(attr(tree_with_icons$folder1, "sttype"), "directory")
+  expect_equal(attr(tree_with_icons$folder1$subfolder1, "sttype"), "directory")
+  expect_equal(attr(tree_with_icons$folder2, "sttype"), "directory")
+})
