de0f20245065d3024edd00bbaa37239ac0fc0a4f
Sandro Raabe <s.raabe@consus-clinicmanagement.de>
2021-03-31 10:01:01 +0200

    translation

--- R/tree_traversal.R
+++ R/tree_traversal.R
@@ -8,14 +8,13 @@
 #' @param inner_val value of attribute for inner tree nodes
 #' @param leaf_val value of attribute for outer tree nodes
 #' @examples
-#'\dontrun{
 #' tree <- dfToTree(data.frame(Titanic),  c("Sex", "Survived"))
 #' str(set_node_attrs(tree, attr_name = "sttype", inner_val = "directory", leaf_val = "file"))
-#'}
+#'
 #' @return named nested list
 #' @export
 set_node_attrs <- function(tree, attr_name, inner_val, leaf_val){
-  if(length(tree) == 0 || is.atomic(tree)){ # trifft leaves der Form "" oder list()
+  if(length(tree) == 0 || is.atomic(tree)){ # matches leaves like "" or list() (which comes from dfToTree)
     set_attr(tree, attr_name, leaf_val)
   }else{
     res <- lapply(tree, set_node_attrs, attr_name, inner_val, leaf_val)
